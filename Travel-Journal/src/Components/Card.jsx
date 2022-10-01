6069f6815f8d2c96cabab9d458994c2692510d24
KingLyrics <ekom338.com@gmail.com>
2022-10-01 18:36:17 +0100

    Card started. Still in development stages

--- Travel-Journal/src/Components/Card.jsx
+++ Travel-Journal/src/Components/Card.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 function Card() {
   return (
-    <div>this is now a card</div>
+    <section className='row'>
+        <div className='img'>
+            <img src="./Images/Burjh1.jpg" alt="" />
+        </div>
+
+        <div className="info-block">
+          
+          <div className="title">
+          <h3>Dubai</h3>
+          <a href="">View on Google Maps</a>
+          </div>
+
+        <div className="head">
+          <h1>The Burjh Khalifa</h1>
+        </div>
+
+        </div>
+          
+
+
+    </section>
   )
 }
 
